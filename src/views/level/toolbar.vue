49eb312b52e38cf1123e19e05ef832228fd087f4
biewxw <gabrieldeoliveira.gdo23@gmail.com>
2020-07-09 05:08:23 -0300

    (feat) added level view

---
+++ src/views/level/toolbar.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="toolbar-wrapper">
+    <div class="toolbar-wrapper--icon" @click="onGoToLevels">
+      <i class="fas fa-chevron-left"/>
+    </div>
+    <div class="toolbar-wrapper--life">
+      <div
+        v-for="(item, index) in lives"
+        class="life--icon"
+        :class="{'incomplete': hasLostLife(item)}"
+        :key="index" />
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex';
+
+export default {
+  name: 'Toolbar',
+
+  data: () => ({
+    lives: Array.from({ length: 5 }, (x, y) => y + 1),
+  }),
+
+  computed: {
+    ...mapState(['userLife']),
+  },
+
+  methods: {
+    hasLostLife(value) {
+      return value > this.userLife;
+    },
+
+    onGoToLevels() {
+      const { name, params } = this.$route;
+
+      if (name === 'challenge') {
+        this.$router.replace({ name: 'challenges', params: { id: params.id } });
+      } else {
+        this.$router.replace({ name: 'levels' });
+      }
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.toolbar-wrapper {
+  display: grid;
+  grid-template-columns: 22px 1fr 22px;
+  grid-template-rows: 22px;
+  justify-content: center;
+  align-items: center;
+
+  &--icon {
+    color: #401C9C;
+    font-size: 22px;
+    transition: 0.3s;
+    cursor: pointer;
+
+    &:hover {
+      color: #33177a;
+    }
+  }
+
+  &--life {
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    .life--icon {
+      width: 10px;
+      height: 10px;
+      margin: 0px 5px;
+      border-radius: 100%;
+      background: #401C9C;
+      border: 1px solid #401C9C;
+
+      &.incomplete {
+        background: #080525;
+      }
+    }
+
+  }
+}
+</style>
